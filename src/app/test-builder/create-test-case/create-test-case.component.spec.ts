9287d634f65dda099b0469f4497d8d45055d4a4b
hesoyam4ik <96014223+hesoyam4ik@users.noreply.github.com>
2025-05-07 20:26:55 +0300

    some improvments

---
+++ src/app/test-builder/create-test-case/create-test-case.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateTestCaseComponent } from './create-test-case.component';
+
+describe('CreateTestCaseComponent', () => {
+  let component: CreateTestCaseComponent;
+  let fixture: ComponentFixture<CreateTestCaseComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateTestCaseComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreateTestCaseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/test-builder/test-builder.module.ts
+++ src/app/test-builder/test-builder.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TestBuilderComponent} from "./test-builder.component";
-import {RouterModule, Routes} from "@angular/router";
+import { TestBuilderComponent } from "./test-builder.component";
+import { RouterModule, Routes } from "@angular/router";
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { allIcons, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { CreateTestCaseComponent } from './create-test-case/create-test-case.component';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+
 
 
 const routes: Routes = [
@@ -13,14 +21,19 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    TestBuilderComponent
+    TestBuilderComponent,
+    CreateTestCaseComponent
   ],
   exports: [
     TestBuilderComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatStepperModule,
+    MatIconModule,
+    NgxBootstrapIconsModule.pick(allIcons),
+    MatDialogModule
   ],
 })
 export class TestBuilderModule { }
